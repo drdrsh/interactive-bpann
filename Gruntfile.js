8bb7e839a60e33627d8d6b9385a69661776305b7
drdrsh <mostafa.mohmmed@gmail.com>
2016-02-01 09:45:03 -0600

    Build process cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,23 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+        
+        "bower-install-simple": {
+            options: {
+                color: true 
+            },
+            "prod": {
+                options: {
+                    production: true
+                }
+            },
+            "dev": {
+                options: {
+                    production: false
+                }
+            }
+        },
+        
         concat: {
             
             "jquery-ui": {
@@ -29,63 +46,63 @@ module.exports = function(grunt) {
                 dest: 'bower_components/jquery-ui/jquery-ui.js'
             },
             
-            sigma: {
+            "sigma.js": {
                 options: {
                       separator: ';',
                 },
                 src: [
-                    "bower_components/sigma/src/sigma.core.js",
-                    "bower_components/sigma/src/conrad.js",
-                    "bower_components/sigma/src/utils/sigma.utils.js",
-                    "bower_components/sigma/src/utils/sigma.polyfills.js",
-                    "bower_components/sigma/src/sigma.settings.js",
-                    "bower_components/sigma/src/classes/sigma.classes.dispatcher.js",
-                    "bower_components/sigma/src/classes/sigma.classes.configurable.js",
-                    "bower_components/sigma/src/classes/sigma.classes.graph.js",
-                    "bower_components/sigma/src/classes/sigma.classes.camera.js",
-                    "bower_components/sigma/src/classes/sigma.classes.quad.js",
-                    "bower_components/sigma/src/classes/sigma.classes.edgequad.js",
-                    "bower_components/sigma/src/captors/sigma.captors.mouse.js",
-                    "bower_components/sigma/src/captors/sigma.captors.touch.js",
-                    "bower_components/sigma/src/renderers/sigma.renderers.canvas.js",
-                    "bower_components/sigma/src/renderers/sigma.renderers.webgl.js",
-                    "bower_components/sigma/src/renderers/sigma.renderers.svg.js",
-                    "bower_components/sigma/src/renderers/sigma.renderers.def.js",
-                    "bower_components/sigma/src/renderers/webgl/sigma.webgl.nodes.def.js",
-                    "bower_components/sigma/src/renderers/webgl/sigma.webgl.nodes.fast.js",
-                    "bower_components/sigma/src/renderers/webgl/sigma.webgl.edges.def.js",
-                    "bower_components/sigma/src/renderers/webgl/sigma.webgl.edges.fast.js",
-                    "bower_components/sigma/src/renderers/webgl/sigma.webgl.edges.arrow.js",
-                    "bower_components/sigma/src/renderers/canvas/sigma.canvas.labels.def.js",
-                    "bower_components/sigma/src/renderers/canvas/sigma.canvas.hovers.def.js",
-                    "bower_components/sigma/src/renderers/canvas/sigma.canvas.nodes.def.js",
-                    "bower_components/sigma/src/renderers/canvas/sigma.canvas.edges.def.js",
-                    "bower_components/sigma/src/renderers/canvas/sigma.canvas.edges.curve.js",
-                    "bower_components/sigma/src/renderers/canvas/sigma.canvas.edges.arrow.js",
-                    "bower_components/sigma/src/renderers/canvas/sigma.canvas.edges.curvedArrow.js",
-                    "bower_components/sigma/src/renderers/canvas/sigma.canvas.edgehovers.def.js",
-                    "bower_components/sigma/src/renderers/canvas/sigma.canvas.edgehovers.curve.js",
-                    "bower_components/sigma/src/renderers/canvas/sigma.canvas.edgehovers.arrow.js",
-                    "bower_components/sigma/src/renderers/canvas/sigma.canvas.edgehovers.curvedArrow.js",
-                    "bower_components/sigma/src/renderers/canvas/sigma.canvas.extremities.def.js",
-                    "bower_components/sigma/src/renderers/svg/sigma.svg.utils.js",
-                    "bower_components/sigma/src/renderers/svg/sigma.svg.nodes.def.js",
-                    "bower_components/sigma/src/renderers/svg/sigma.svg.edges.def.js",
-                    "bower_components/sigma/src/renderers/svg/sigma.svg.edges.curve.js",
-                    "bower_components/sigma/src/renderers/svg/sigma.svg.labels.def.js",
-                    "bower_components/sigma/src/renderers/svg/sigma.svg.hovers.def.js",
-                    "bower_components/sigma/src/middlewares/sigma.middlewares.rescale.js",
-                    "bower_components/sigma/src/middlewares/sigma.middlewares.copy.js",
-                    "bower_components/sigma/src/misc/sigma.misc.animation.js",
-                    "bower_components/sigma/src/misc/sigma.misc.bindEvents.js",
-                    "bower_components/sigma/src/misc/sigma.misc.bindDOMEvents.js",
-                    "bower_components/sigma/src/misc/sigma.misc.drawHovers.js",
-                    "bower_components/sigma/plugins/sigma.renderers.edgeLabels/settings.js",
-                    "bower_components/sigma/plugins/sigma.renderers.edgeLabels/sigma.canvas.edges.labels.def.js",
-                    "bower_components/sigma/plugins/sigma.renderers.edgeLabels/sigma.canvas.edges.labels.curve.js",
-                    "bower_components/sigma/plugins/sigma.renderers.edgeLabels/sigma.canvas.edges.labels.curvedArrow.js"
+                    "bower_components/sigma.js/src/sigma.core.js",
+                    "bower_components/sigma.js/src/conrad.js",
+                    "bower_components/sigma.js/src/utils/sigma.utils.js",
+                    "bower_components/sigma.js/src/utils/sigma.polyfills.js",
+                    "bower_components/sigma.js/src/sigma.settings.js",
+                    "bower_components/sigma.js/src/classes/sigma.classes.dispatcher.js",
+                    "bower_components/sigma.js/src/classes/sigma.classes.configurable.js",
+                    "bower_components/sigma.js/src/classes/sigma.classes.graph.js",
+                    "bower_components/sigma.js/src/classes/sigma.classes.camera.js",
+                    "bower_components/sigma.js/src/classes/sigma.classes.quad.js",
+                    "bower_components/sigma.js/src/classes/sigma.classes.edgequad.js",
+                    "bower_components/sigma.js/src/captors/sigma.captors.mouse.js",
+                    "bower_components/sigma.js/src/captors/sigma.captors.touch.js",
+                    "bower_components/sigma.js/src/renderers/sigma.renderers.canvas.js",
+                    "bower_components/sigma.js/src/renderers/sigma.renderers.webgl.js",
+                    "bower_components/sigma.js/src/renderers/sigma.renderers.svg.js",
+                    "bower_components/sigma.js/src/renderers/sigma.renderers.def.js",
+                    "bower_components/sigma.js/src/renderers/webgl/sigma.webgl.nodes.def.js",
+                    "bower_components/sigma.js/src/renderers/webgl/sigma.webgl.nodes.fast.js",
+                    "bower_components/sigma.js/src/renderers/webgl/sigma.webgl.edges.def.js",
+                    "bower_components/sigma.js/src/renderers/webgl/sigma.webgl.edges.fast.js",
+                    "bower_components/sigma.js/src/renderers/webgl/sigma.webgl.edges.arrow.js",
+                    "bower_components/sigma.js/src/renderers/canvas/sigma.canvas.labels.def.js",
+                    "bower_components/sigma.js/src/renderers/canvas/sigma.canvas.hovers.def.js",
+                    "bower_components/sigma.js/src/renderers/canvas/sigma.canvas.nodes.def.js",
+                    "bower_components/sigma.js/src/renderers/canvas/sigma.canvas.edges.def.js",
+                    "bower_components/sigma.js/src/renderers/canvas/sigma.canvas.edges.curve.js",
+                    "bower_components/sigma.js/src/renderers/canvas/sigma.canvas.edges.arrow.js",
+                    "bower_components/sigma.js/src/renderers/canvas/sigma.canvas.edges.curvedArrow.js",
+                    "bower_components/sigma.js/src/renderers/canvas/sigma.canvas.edgehovers.def.js",
+                    "bower_components/sigma.js/src/renderers/canvas/sigma.canvas.edgehovers.curve.js",
+                    "bower_components/sigma.js/src/renderers/canvas/sigma.canvas.edgehovers.arrow.js",
+                    "bower_components/sigma.js/src/renderers/canvas/sigma.canvas.edgehovers.curvedArrow.js",
+                    "bower_components/sigma.js/src/renderers/canvas/sigma.canvas.extremities.def.js",
+                    "bower_components/sigma.js/src/renderers/svg/sigma.svg.utils.js",
+                    "bower_components/sigma.js/src/renderers/svg/sigma.svg.nodes.def.js",
+                    "bower_components/sigma.js/src/renderers/svg/sigma.svg.edges.def.js",
+                    "bower_components/sigma.js/src/renderers/svg/sigma.svg.edges.curve.js",
+                    "bower_components/sigma.js/src/renderers/svg/sigma.svg.labels.def.js",
+                    "bower_components/sigma.js/src/renderers/svg/sigma.svg.hovers.def.js",
+                    "bower_components/sigma.js/src/middlewares/sigma.middlewares.rescale.js",
+                    "bower_components/sigma.js/src/middlewares/sigma.middlewares.copy.js",
+                    "bower_components/sigma.js/src/misc/sigma.misc.animation.js",
+                    "bower_components/sigma.js/src/misc/sigma.misc.bindEvents.js",
+                    "bower_components/sigma.js/src/misc/sigma.misc.bindDOMEvents.js",
+                    "bower_components/sigma.js/src/misc/sigma.misc.drawHovers.js",
+                    "bower_components/sigma.js/plugins/sigma.renderers.edgeLabels/settings.js",
+                    "bower_components/sigma.js/plugins/sigma.renderers.edgeLabels/sigma.canvas.edges.labels.def.js",
+                    "bower_components/sigma.js/plugins/sigma.renderers.edgeLabels/sigma.canvas.edges.labels.curve.js",
+                    "bower_components/sigma.js/plugins/sigma.renderers.edgeLabels/sigma.canvas.edges.labels.curvedArrow.js"
                 ],
-                dest: 'bower_components/sigma/sigma.js',
+                dest: 'bower_components/sigma.js/sigma.js',
             }
         },
         uglify: {
@@ -107,10 +124,10 @@ module.exports = function(grunt) {
                 files: {
                     "source/assets/lib/jquery/jquery.js" : "./jquery/dist/jquery.js",
                     "source/assets/lib/jquery-ui/jquery-ui.js" : "./jquery-ui/jquery-ui.js",
-                    "source/assets/lib/jquery-ui/jquery-ui.css" : "./jquery-ui/themes/ui-lightness/jquery-ui.css",
-                    "source/assets/lib/jquery-ui/images" : "./jquery-ui/themes/ui-lightness/images",
+                    "source/assets/lib/jquery-ui/jquery-ui.css" : "./jquery-ui/themes/start/jquery-ui.css",
+                    "source/assets/lib/jquery-ui/images" : "./jquery-ui/themes/start/images",
                     "source/assets/lib/jquery-ui/jquery-ui.js" : "./jquery-ui/jquery-ui.js",
-                    "source/assets/lib/sigma/sigma.js" : "./sigma/sigma.js",
+                    "source/assets/lib/sigma/sigma.js" : "./sigma.js/sigma.js",
                     "source/assets/lib/w2ui/w2ui.js" : "./w2ui/w2ui-1.4.3.js",
                     "source/assets/lib/w2ui/w2ui.css" : "./w2ui/w2ui-1.4.3.css",
                     
@@ -157,6 +174,7 @@ module.exports = function(grunt) {
         }
     });
     
+    grunt.loadNpmTasks("grunt-bower-install-simple");
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
@@ -165,5 +183,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-serve');
     
     // task setup 
-    grunt.registerTask('default', ['concat', 'jshint', 'bowercopy', 'htmlbuild', 'uglify']);
-};
+    grunt.registerTask('default', ['bower-install-simple', 'concat', 'jshint', 'bowercopy', 'htmlbuild', 'uglify']);
+}; 
--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-# interactive-bpann
-A web-based, Interactive, step-by-step Back-propagate Artificial neural network simulator.
+# A web-based interactive visualization of artificial neural networks
 
-This demo uses some ECMAScript 6 and 7 features as well as web workers to provide a clean, step-by-step implementation of Backprogragate Artificial neural network primarily for educational purposes.
+This demo aims to serve as a tool to help visual learners, like myself, to gain visual intuition as to how artificial neural networks work. This demo allows a step-by-step iteration through a fully-connected backpropagation artificial neural network network to get some intuition as to what goes on during the process of training. It uses the latest Javascript technologies, particularly the "yield" keyword that allows clean loop pause and resume. The default demo attempts to learn an XOR problem.
+
+**The demo is experimental and it uses the latest ES6 features so it won't work on all browsers, also the performance still needs optimization so I recommend running it in Google Chrome.**
+
+The code still needs cleanup and further testing so issue reports and pull request are most welcome. The code is live on http://experiments.mostafa.io/public/ffbpann/
+
+
+# How to install locally
+
+To run this demo on your local machine (technically, it is a client side so it will always run on your machine, but you get the idea), you will need to have [nodejs](https://nodejs.org/en/), [npm](https://www.npmjs.com/), [grunt](http://gruntjs.com) and [bower](http://bower.io/) installed.
+
+After that clone the repository to your local system.
+
+    git clone https://github.com/drdrsh/interactive-bpann
+
+navigate to the path where the code resides and run
+
+    npm install
+    grunt
+    grunt serve
+
+This will install all the dependancies and run grunt server on port 9000 
+
+    http://localhost:9000/index.html
+    
